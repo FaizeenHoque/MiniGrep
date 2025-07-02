87b32363dcf8e72eaeaa83682343eb7a8f7fa42b
FaizeenHoque <haquefaizeen11@gmail.com>
2025-07-02 21:40:12 +0600

    Accepting command arguments from user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
+
+    println!("Searching for {} in {}", query, filename);
 }
